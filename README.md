4aa878c4a600a6f468c9daf5bad4ce3f0a3cd0a0
Richard Tynan <richard@tynan.me>
2016-01-23 15:52:59 +0000

    Directory structure, examples and codestyle checking

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # IBProject
+
+## Style Checking
+Run the bash script `./checkstyle/check.sh <files to check>`, or alternatively import `checkstyle/google_checks.xml` into your own style-checker.
+
---
+++ uk/ac/cam/quebec/wikiproc/Example.java
@@ -0,0 +1,32 @@
+package uk.ac.cam.quebec.trends;
+
+import com.xyz;
+
+public class Trends {
+
+    /**
+     * Validates a chess move.
+     *
+     * <p>Use {@link #doMove(int theFromFile, int theFromRank, int theToFile,
+     * int theToRank)} to move a piece.
+     *
+     * @param theFromFile file from which a piece is being moved
+     * @param theFromRank rank from which a piece is being moved
+     * @param theToFile   file to which a piece is being moved
+     * @param theToRank   rank to which a piece is being moved
+     * @return            true if the move is valid, otherwise false
+     */
+    boolean isValidMove(int theFromFile, int theFromRank, int theToFile, int theToRank) {
+        // ...body
+    }
+
+    /**
+     * Moves a chess piece.
+     *
+     * @see java.math.RoundingMode
+     */
+    void doMove(int theFromFile, int theFromRank, int theToFile, int theToRank)  {
+        // ...body
+    }
+
+}
