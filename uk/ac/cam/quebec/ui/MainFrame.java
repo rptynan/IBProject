b3cbf5fe73ffb499f5f8a29ed6aa39cdfa0b2c65
stuartgrigg <stuartgrigg1@gmail.com>
2016-01-31 23:16:25 +0000

    Possible start on a UI

---
+++ uk/ac/cam/quebec/ui/MainFrame.java
@@ -0,0 +1,48 @@
+package uk.ac.cam.quebec.ui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * Base frame for a Swing UI.
+ * @author Stuart
+ *
+ */
+public class MainFrame extends JFrame {
+
+    private JPanel contentPane;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MainFrame frame = new MainFrame();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public MainFrame() {
+        super("Twik Feed");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        setContentPane(contentPane);
+    }
+
+}
