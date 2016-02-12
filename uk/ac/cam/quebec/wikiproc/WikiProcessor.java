377295c66b740109aa70cc34e5afd0a6839e53d6
Tudor Tiplea <tudor.tiplea96@gmail.com>
2016-02-12 18:12:41 +0000

    Fixed KG Errors for now by limiting WikiProcess to 10 concepts/trend and adding exponential backoff retries to KG generator. Tested entire project and works fine

--- uk/ac/cam/quebec/wikiproc/WikiProcessor.java
+++ uk/ac/cam/quebec/wikiproc/WikiProcessor.java
@@ -26,6 +26,8 @@ import javafx.util.Pair;
  */
 public class WikiProcessor {
 
+    private static final int CONCEPTS_LIMIT = 10;
+
     private Trend trend;
     private List<WikiArticle> articleList;
     private List<Pair<String, Double>> wikiConcepts;
@@ -82,7 +84,12 @@ public class WikiProcessor {
         }
 
         // get KGConcepts for each trendConcept and put them all together
+        int limit = CONCEPTS_LIMIT;
         for (Pair<String, Integer> trendConcept : trendConcepts) {
+            --limit;
+            if (limit < 0) {
+                break;
+            }
             for (KGConcept kgConcept : kgConceptGenerator.getKGConcepts(trendConcept.getKey(),
                     trendConcept.getValue() >= averageCount ? 2 : 1)) {
                 if (kgConcept != null) {
